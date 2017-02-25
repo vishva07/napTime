1ee98e33cbefec3f66232402e77dd90f891780e2
Ashish Sinha <ashishsinha108@gmail.com>
2017-02-25 11:01:33 +0530

    alert window added

--- take_a_break.py
+++ take_a_break.py
@@ -1,7 +1,18 @@
 import time
 import webbrowser
+import os
+import tkMessageBox
 i=1
-while (i<=5):
+path='C:\Users\LENOVO\Desktop\Udacity\Python'
+file_list=os.listdir(path)
+file=file_list[0]
+
+while (i<4):
+    print("Just wait for 2 hours. Do Your Job!")
+    print("An alert sound will be played")
+
     time.sleep(2*60*60)
+    os.startfile(path+"/"+file)
+    tkMessageBox.showinfo(title="Alert", message="Stop Working! Relax Now!!")
     webbrowser.open("https://www.youtube.com/watch?v=xwtdhWltSIg")
     i=i+1
